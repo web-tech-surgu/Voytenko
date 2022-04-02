edb0b4b937cac46a1daf7eb855b108010bd08e47
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-02 14:05:47 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?
-echo "Hello world"
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Сайт</title>
+<link rel="stylesheet" type="text/css" href="stayle.css" />
+</head>
+<body>
+<div id="categories">
+</div>
+<div id="conteiner">
+<div id="objects">
+</div>
+<div id="content">
+</div>
+</div>
+</body>
+</html>
