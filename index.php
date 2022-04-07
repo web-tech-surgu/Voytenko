ed0556efd9f0b75d1ee57fc2d08f0b00b2a63be7
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 10:34:40 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 			  Категории
 			  
 			  <ul id="list">
-				<li><a hreft="index.php? C=1"> Категория 1</a></li>
-				<li><a hreft="index.php? C=2"> Категория 2</a></li>
-				<li><a hreft="index.php? C=3"> Категория 3</a></li>
+				<li><a hreft="index.php?C=1"> Категория 1</a></li>
+				<li><a hreft="index.php?C=2"> Категория 2</a></li>
+				<li><a hreft="index.php?C=3"> Категория 3</a></li>
 				<li><a hreft="index.php"> Домой</a></li>
-				<li><a hreft="index.php? 0=99"> Форма</a></li>
+				<li><a hreft="index.php?O=99"> Форма</a></li>
 			  </ul>    
 			</div>
 			<div id="conteiner">
