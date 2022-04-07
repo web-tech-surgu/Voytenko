ee84b18c8a58a593df00bc2fae326d45449329c8
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 10:14:50 +0500

    Update index.php

--- index.php
+++ index.php
@@ -18,31 +18,10 @@
 			</div>
 			<div id="conteiner">
 					<div id="objects">
-					  <?php 
-						 $C_GET["C"];
-						   switch($C)  {
-							 case "1": echo(file_get_contents("objects1.html")); break;
-							 case "2": echo(file_get_contents("objects2.html")); break;
-							 case "3": echo(file_get_contents("objects3.html")); break;
-							 default:echo(file_get_contents("about.html"));}
-					   ?>
+					 
 						</div>
 						<div id="content">
-						<?php
-							$0=$_GET["0"];
-							switch($0) {		     
-							 case "1-1": echo(file_get_contents("objects1-1.html")); break;
-							 case "1-2": echo(file_get_contents("objects1-2.html")); break;
-							 case "1-3": echo(file_get_contents("objects1-3.html")); break;
-							 case "2-1": echo(file_get_contents("objects2-1.html")); break;
-							 case "2-2": echo(file_get_contents("objects2-2.html")); break;
-							 case "2-3": echo(file_get_contents("objects2-3.html")); break;
-							 case "3-1": echo(file_get_contents("objects3-1.html")); break;
-							 case "3-2": echo(file_get_contents("objects3-2.html")); break;
-							 case "3-3": echo(file_get_contents("objects3-3.html")); break;
-							 case "99": include("form_lab6.php");  break;
-							 default: echo(file_get_contents("applicate.html"));}
-						?>
+				
 					</div>
 				</div>
 		</body>
