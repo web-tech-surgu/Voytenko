bf35ac2f9e42b61366a17e1ad64afd08caf944b4
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 09:21:21 +0500

    Update index.php

--- index.php
+++ index.php
@@ -14,16 +14,45 @@
     <li><a hreft="index.php? C=3"> Категория 3</a></li>
     <li><a hreft="index.php"> Домой</a></li>
     <li><a hreft="index.php? 0=99"> Форма</a></li>
-  </ul>
-      
+  </ul>    
 </div>
 <div id="conteiner">
 <div id="objects">
-  Объекты
-</div>
-<div id="content">
-  Описание объектов
-</div>
-</div>
-</body>
+ <?php 
+ $C_GET["C"];
+
+				switch($C)
+				{
+            
+				case "1": echo(file_get_contents("objects1.html"));  break;
+          case "2": echo(file_get_contents("objects2.html"));    break;
+				case "3": echo(file_get_contents("objects3.html"));     break;
+          default:echo(file_get_contents("about.html"));)
+       ?>
+  </div>
+  <div id="content">
+    <?php
+          $0=$_GET["0"];
+            switch($0)
+				{
+     		case "1-1": echo(file_get_contents("objects1-1.html"));  break;
+				case "1-2": echo(file_get_contents("objects1-2.html"));  break;
+				case "1-3": echo(file_get_contents("objects1-3.html"));  break;
+				case "2-1": echo(file_get_contents("objects2-1.html"));  break;
+				case "2-2": echo(file_get_contents("objects2-2.html"));  break;
+        case "2-3": echo(file_get_contents("objects2-3.html"));  break;
+				case "3-1": echo(file_get_contents("objects3-1.html"));  break;
+				case "3-2": echo(file_get_contents("objects3-2.html"));  break;
+				case "3-3": echo(file_get_contents("objects3-3.html"));  break;
+        case "99": include("form_lab6.php");  break;
+        default:echo(file_get_contents("applicate.html"));)
+             ?>
+  </div>
+  </div>
+  </body>
 </html>
+
+  
+				}
+				
+				}
