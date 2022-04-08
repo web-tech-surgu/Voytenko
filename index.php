2dfbf8eabc8603f11e6c5fd9817e0d833706cdc4
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-08 10:45:21 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html>
-		<head>
-		 <title>Лабораторная работа 7. Динамическая генерация web-страниц на основе шаблона</title>
-		 <link rel="stylesheet" type="text/css" href="stayle.css" />
-		</head>
-		<body>
-			<div id="categories">
-			  Категории
-			  
-			  <ul id="list">
-				<a href="lab6.php?C=1"> Категория 1</a>
-				<a href="lab6.php?C=2"> Категория 2</a>
-				<a href="lab6.php?C=3"> Категория 3</a>
-				<a href="lab6.php"> Домой</a>
-			 </div>
-				<div id="conteiner">
-					<div id="objects">
-			{objects}
-					</div>
-					<div id="content">
-			{content}
-		        		</div>
-				</div>
-		</body>
+
 <?php
 $tmp=file_get_contents("lab7.tpl");  // Загрузка шаблона впеременную
 $objects="";  // Очистка переменных
